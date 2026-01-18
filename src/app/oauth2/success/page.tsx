290216a42594661b370d4a7669a98c02aa854466
unknown <dbsgywls667@gmail.com>
2026-01-19 03:51:08 +0900

    아이디 체크추가\

--- src/app/oauth2/success/page.tsx
+++ src/app/oauth2/success/page.tsx
@@ -1,37 +1,61 @@
+// "use client";
+
+
+// import { useSearchParams } from "next/navigation";
+// import { useEffect } from "react";
+// import { useRouter } from "next/navigation";
+
+
+
+// export default function OAuth2SuccessPage() {
+//   const params = new URLSearchParams(window.location.search);
+//   const token = params.get("token");
+//   const hasPhone = params.get("hasPhone") === "true";
+//   const router = useRouter();
+//   console.log("hasPhone", hasPhone);
+//   useEffect(() => {
+
+
+//     if (token) {
+//       localStorage.setItem("token", token);
+
+//       if (hasPhone) {
+//         // 휴대폰 번호 있음 → 홈으로
+//         // window.location.href = "/";
+//         router.push("/");
+//       } else {
+//         // 휴대폰 번호 없음 → 추가 입력 페이지
+//         // window.location.href = "/register/naverLogin";
+//         router.push("/register/kakaoLogin");
+//       }
+//     }
+//   }, [token]);
+
+//   return <p>로그인 처리 중입니다...</p>;
+// }
 "use client";
 
-
-import { useSearchParams } from "next/navigation";
+import { useSearchParams, useRouter } from "next/navigation";
 import { useEffect } from "react";
-import { useRouter } from "next/navigation";
-
-
 
 export default function OAuth2SuccessPage() {
-  // const searchParams = useSearchParams();
-  // const token = searchParams.get("token");
-  const params = new URLSearchParams(window.location.search);
-  const token = params.get("token");
-  const hasPhone = params.get("hasPhone") === "true";
+  const searchParams = useSearchParams();
   const router = useRouter();
-  console.log("hasPhone", hasPhone);
+
+  const token = searchParams.get("token");
+  const hasPhone = searchParams.get("hasPhone") === "true";
+
   useEffect(() => {
+    if (!token) return;
 
+    localStorage.setItem("token", token);
 
-    if (token) {
-      localStorage.setItem("token", token);
-
-      if (hasPhone) {
-        // 휴대폰 번호 있음 → 홈으로
-        // window.location.href = "/";
-        router.push("/");
-      } else {
-        // 휴대폰 번호 없음 → 추가 입력 페이지
-        // window.location.href = "/register/naverLogin";
-        router.push("/register/kakaoLogin");
-      }
+    if (hasPhone) {
+      router.push("/");
+    } else {
+      router.push("/register/kakaoLogin");
     }
-  }, [token]);
+  }, [token, hasPhone, router]);
 
   return <p>로그인 처리 중입니다...</p>;
 }
