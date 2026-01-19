f351c2155f1e38b74e1ca767a19f05a142597b6c
unknown <dbsgywls667@gmail.com>
2026-01-19 12:57:23 +0900

    PacsController추가\\

--- next.config.ts
+++ next.config.ts
@@ -5,7 +5,7 @@ const nextConfig: NextConfig = {
     return [
       {
         source: "/api/:path*",
-        destination: "http://54.180.149.63:8080/:path*",
+        destination: "http://54.180.149.63/:path*",
       },
     ];
   },
